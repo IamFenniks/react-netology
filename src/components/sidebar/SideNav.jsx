cad304ae086428aef3d363a7dcabc248994ea11c
IamFennics <andrej.darmenko@gmail.com>
2022-12-26 20:17:42 +0300

    Left menu some corrected

--- src/components/sidebar/SideNav.jsx
+++ src/components/sidebar/SideNav.jsx
@@ -6,10 +6,10 @@ const SideNav = () => {
     return (
         <div className="side_nav">
             <nav>
-                <div><Link to='shopitem'>Карточка товара</Link></div>
-                <div><Link to='calendar'>Календарь</Link></div>
-                <div><Link to='portfolio'>Портфолио</Link></div>
-                <div><Link to='store'>Магазин</Link></div>
+                <div>&#x26C4; <Link to='shopitem'>Карточка товара</Link></div>
+                <div>&#x26C4; <Link to='calendar'>Календарь</Link></div>
+                <div>&#x26C4; <Link to='portfolio'>Портфолио</Link></div>
+                <div>&#x26C4; <Link to='store'>Магазин</Link></div>
             </nav>
         </div>
     );
