ff5f25e0e65afd6be2ce8783a73210318a31be1b
adminandrej <andrej.darmenko@gmail.com>
2023-01-10 07:56:42 +0300

    HW 4/1 continue...

---
+++ src/components/hw2/dropdown/DropdownList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DropdownItem from "./DropdownItem";
+
+const DropdownList = ({ list }) => {
+    return (
+        <ul data-id="dropdown" className="dropdown">
+            { list.map(item=> <DropdownItem key={item} item={ item } /> )}
+        </ul>
+    );
+}
+
+export default DropdownList;
--- src/components/hw4/hex2rgb/Hex2Rgb.jsx
+++ src/components/hw4/hex2rgb/Hex2Rgb.jsx
@@ -1,23 +1,73 @@
-import React from "react";
-const handleSubmit = (e) => {
-    e.preventDefault();
-}
-let handleHex = () => {
+import React, { useState } from "react";
+import styled from "styled-components";
 
-}
-let handleRgb = () => {
+let ColorWrepper = styled.div`
+    background-color: ${props => props.inputColor}
+`
 
-}
+// const handleSubmit = (e) => {
+//     e.preventDefault();
+// }
+
 
 const Hex2Rgb = () => {
+    
+    const [form, setForm] = useState({
+        hexName: '#34495e',
+        rgbName: ''
+    });
+    let handleHexChange = (e) => {
+        setForm(prevForm => ({...prevForm, hexName: e.target.value}));
+    
+        // var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(form.hexName);
+        // result = result ? {
+        //     r: parseInt(result[1], 16),
+        //     g: parseInt(result[2], 16),
+        //     b: parseInt(result[3], 16)
+        // } : 'Ошибка';
+    }
+
+    function getRGB(color){
+        if(color.length == 7){
+          var r = parseInt(color.substr(1,2),16);
+          var g = parseInt(color.substr(3,2),16);
+          var b = parseInt(color.substr(5,2),16);    
+          return 'rgb('+r+','+g+','+b+')' ;
+        }    
+        else
+          console.log('Enter correct value');
+      }
+      var a = getRGB('#f0f0f0');
+      if(!a){
+       a = 'Enter correct value'; 
+      
+      setForm(prevForm => ({...prevForm, rgbName: a}))
+      }
+
     return (
         <div className="m-content">
-            <div className="color-wrapper">
-                <form className="input-box" onSubmit={handleSubmit}>
-                    <div><input type="text" className="hex" name="hex" onChange={handleHex} value="#eh32lf" /></div>
-                    <div><input type="text" className="rgb" name="rgb" onChange={handleRgb} value="#eh32lf" /></div>
+            <ColorWrepper className="color-wrapper" inputColor={form.hexName}>
+                <form className="input-box">
+                    <div>
+                        <input 
+                            type="text" 
+                            className="hex" 
+                            name="hex" 
+                            onChange={handleHexChange} 
+                            value={form.hexName}
+                        />
+                    </div>
+                    
+                    <div>
+                        <input 
+                            type="text" 
+                            className="rgb" 
+                            name="rgb" 
+                            value={form.rgbName} 
+                        />
+                    </div>
                 </form>
-            </div>
+            </ColorWrepper>
         </div>
     );
 }
