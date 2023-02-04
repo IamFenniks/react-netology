63fedb9b9c71009201eaaad322da56bd55e938a6
adminandrej <andrej.darmenko@gmail.com>
2023-02-04 21:59:06 +0300

    HW 5/3 is stsrted...

--- src/components/hw5/collapse/Collapse.jsx
+++ src/components/hw5/collapse/Collapse.jsx
@@ -1,8 +1,25 @@
 import React from "react";
- const Collapse = () => {
+
+const Collapse = () => {
     return (
         <div className="m-content">
             <h2>Collapse</h2>
+
+            <div class="bd-example">
+                <p>
+                    <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        Link with href
+                    </a>
+                    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        Button with data-bs-target
+                    </button>
+                </p>
+                <div class="collapse" id="collapseExample">
+                    <div class="card card-body">
+                        Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
